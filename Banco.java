447e8dd7fed9d68fd1f6131f9e5ffc106d3c3b49
Daniel Costa <dan.costa.andrade@gmail.com>
2025-09-09 23:31:29 -0300

    Add files via upload

---
+++ Banco.java
@@ -0,0 +1,11 @@
+package principal_danielcosta.banco;
+import principal_danielcosta.tela.ConversorDeMoedas;
+/**
+ *
+ * @author Equipe Vermelha
+ */
+public class Banco {
+    public static void main(String[] args) {
+        ConversorDeMoedas janela = new 
+    }
+}
